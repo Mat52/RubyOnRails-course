b647b1cbf999ea4c02de6c2bed2fc6178119d3e6
Mateusz Łopatkiewicz <mlopatkiewicz@MacBook-Air-Mateusz.local>
2025-08-10 18:56:08 +0200

    Add capacity handling and message out of stock

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -42,7 +42,7 @@ class ProductsController < ApplicationController
       @product = Product.find(params[:id])
     end
     def product_params #added params to handle with form
-      params.expect(product: [ :name, :description, :featured_image ]) 
+      params.expect(product: [ :name, :description, :featured_image, :inventory_count ]) 
     end
 
 end
