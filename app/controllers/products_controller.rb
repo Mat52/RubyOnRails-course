e83ade7f013ce23bb6aee972b89bc931be916a44
Mateusz Łopatkiewicz <mlopatkiewicz@MacBook-Air-Mateusz.local>
2025-08-10 16:11:19 +0200

    Add view to show separate product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,8 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all # instance variable to share view between controllers
   end
+
+  def show
+    @product = Product.find(params[:id])  # Add function to show product with the id specified in request R in CRUD model 
+  end
 end
