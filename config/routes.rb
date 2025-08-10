b8214b14344648bb73772db6634a5a9b1b2fca4e
Mateusz Łopatkiewicz <mlopatkiewicz@MacBook-Air-Mateusz.local>
2025-08-10 16:48:38 +0200

    Add function to create the new product, handle params with form (new/create)

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,25 @@ Rails.application.routes.draw do
 
 
   Rails.application.routes.draw do
-
+  root "products#index"
   resources :products # - shorter version to get all of them in on line
   # rails routes - command to show all routes in application
 
 
   end
+
+ #get "/products", to: "products#index"
+
+#get "/products/new", to: "products#new"
+#post "/products", to: "products#create"
+
+#get "/products/:id", to: "products#show"
+
+#get "/products/:id/edit", to: "products#edit"
+#patch "/products/:id", to: "products#update"
+#put "/products/:id", to: "products#update"
+
+#delete "/products/:id", to: "products#destroy"
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
